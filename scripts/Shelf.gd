76479b9407a561190e2d926c211351cf1b42436d
Luke Foster <lukefoster2019@gmail.com>
2022-02-21 23:39:35 -0500

    started shelf work

---
+++ scripts/Shelf.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+const Book = preload("res://Book.tscn")
+
+export (String) var left_code_letter : String
+export (Vector2) var left_code_bounds : Vector2
+export (String) var right_code_letter : String
+export (Vector2) var right_code_bounds : Vector2
+
+var left_books : Array = []
+var right_books : Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	fill_shelves(left_books, left_code_letter, left_code_bounds)
+	fill_shelves(right_books, right_code_letter, right_code_bounds)
+	 # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+func fill_shelves(arr : Array, code_letter : String, code_bounds : Vector2) -> void:
+	for i in range(code_bounds.x, code_bounds.y):
+		var book : Book = Book.instance()
+		book.book_code = code_letter + str(i)
+		arr.append(book)
--- scripts/book/Book.gd
+++ scripts/book/Book.gd
@@ -1,28 +1,40 @@
 extends Node2D
 
-export(String) var book_title : String
+class_name Book
+
+const CHARS : String = 'abcdefghijklmnopqrstuvwxyz'
+
 export(String) var book_code : String
 
-var characters : String = 'abcdefghijklmnopqrstuvwxyz'
+var book_title : String
+var base_color : Color
+var pages_color : Color
+var corner_tips_color : Color
+var design_color : Color
 
 onready var label : Label = $Title/Label
 onready var sprite_base : Sprite = $Sprite_Base
-onready var sprite_design1 : Sprite = $Sprite_Design1
+onready var sprite_design : Sprite = $Sprite_Design
+onready var sprite_pages : Sprite = $Sprite_Pages
+onready var sprite_cornertips : Sprite = $Sprite_CornerTips
 
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
+func _init() -> void:
+	randomize()
+	book_title = rand_title()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	book_title = rand_title()
 	label.text = book_title
+	color_book()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	pass
 
 func rand_title() -> String:
 	var length : int = 1 + randi() % 2
@@ -35,6 +47,15 @@ func rand_word() -> String:
 	var length : int = 6 + randi() % 6
 	var word : String
 	for i in range(length):
-		word += characters[randi() % 26]
+		word += CHARS[randi() % len(CHARS)]
 	return word
 	
+func color_book() -> void:
+	base_color = Color(rand_range(0.2, 0.8), rand_range(0.2, 0.8), rand_range(0.2, 0.8), 1)
+	pages_color = Color(rand_range(0.85, 1), rand_range(0.85, 1), rand_range(0.85, 1), 1)
+	corner_tips_color = Color(rand_range(0.8, 1), rand_range(0.6, 0.9), rand_range(0, 0.2), 1)
+	design_color = Color(rand_range(0.2, 0.6), rand_range(0.2, 0.6), rand_range(0.2, 0.6), 1)
+	sprite_base.modulate = base_color
+	sprite_pages.modulate = pages_color
+	sprite_cornertips.modulate = corner_tips_color
+	sprite_design.modulate = design_color
